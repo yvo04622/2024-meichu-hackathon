68b1daab135fa887b4233609e837878da9804f1d
yrchen <125744143+yrc40@users.noreply.github.com>
2024-10-19 16:35:57 +0800

    fix: location translation

--- utils.py
+++ utils.py
@@ -135,6 +135,7 @@ def generate_promotion_data(organizer, time, location, event_name, description, 
     主辦單位: {organizer}
     活動時間: {time}
     活動地點: {location}
+    英文的活動地點則是: {replace_location_with_abbrev(location)}
     活動名稱: {event_name}
     活動內容: {description}
     費用: {fee}
